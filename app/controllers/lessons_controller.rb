174907690f39d32e531bb61c91ac86df47081bf3
Alice Duff <duff.alice@gmail.com>
2019-09-08 23:07:37 +0000

    user authentication for lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,20 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :enrolled_in
+
   def show
 
   end
 
   private
+
+  def enrolled_in
+   if current_user && current_user.enrolled_in?(current_lesson.section.course)
+      
+    else
+      redirect_to course_path(current_lesson.section.course), alert: 'Unauthorized User'
+     end
+  end
 
   helper_method :current_lesson
   def current_lesson
